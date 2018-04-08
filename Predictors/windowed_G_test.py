f554c66fff6ded2bab0d668e8b5e50a6cad970fa
gpeh1g14 <gpeh1g14@soton.ac.uk>
2018-04-08 19:40:12 +0100

    IN PROGRESS: Successfully creates dataset of inputs

--- Predictors/windowed_G_test.py
+++ Predictors/windowed_G_test.py
@@ -1,11 +1,16 @@
 from data.import_data import import_data
 from Predictors.windowed_G import windowedGClassifier
 from nltk.tokenize import word_tokenize
+from gensim.models import Word2Vec
 import numpy as np
+import tensorflow as tf
 import pickle
 
 def tokenize(sen):
     return word_tokenize(sen.lower())
+
+ext_encoded = ""
+ext_authors = ""
 
 if __name__ == '__main__':
     tr, te = import_data()
@@ -18,15 +23,38 @@ if __name__ == '__main__':
 
     authors = np.array([author_key[a] for a in authors])
 
-    print(text[:20], authors[:20])
+    encsize = 2
+    window = 5
 
-    # encoder = Word2Vec([
-    #     tokenize(s)
-    #     for s in sentences],
-    #     size=self.encoder_size,
-    #     window=self.window,
-    #     min_count=0,
-    #     workers=4)
+    tokens = [tokenize(s) for s in text]
+
+    encoder = Word2Vec(tokens,
+        size=encsize,
+        window=window,
+        min_count=0,
+        workers=4)
+
+    encoded = np.array([np.array([encoder.wv[w] for w in s]) for s in tokens])
+
+    ext_encoded = []
+    ext_authors = []
+
+    for s, a in zip(encoded, authors):
+        for i in range(len(s) + 1 - window):
+            ext_encoded.append(s[i:i + window])
+            ext_authors.append(a)
+
+    ext_encoded = np.array(ext_encoded)
+    ext_authors = np.array(ext_authors)
+
+    #print(encoded[0])
+
+    dataset = tf.data.Dataset.from_tensor_slices((ext_encoded, ext_authors))
+    iterator = dataset.make_one_shot_iterator()
+    el = iterator.get_next()
+
+    with tf.Session() as sess:
+         print(sess.run(el))
 
     # myc = windowedGClassifier(encoder_size=30, DNNlayers=[30, 20], batch_n = 3)
     # myc.fit(tr.text[0:2], tr.author[0:1])
