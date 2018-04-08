bde53c865d32afefad6040fff8b41999255f7b04
gpeh1g14 <gpeh1g14@soton.ac.uk>
2018-04-08 19:08:55 +0100

    IN PROGRESS: Adds windowed_data.py

--- Predictors/windowed_G_test.py
+++ Predictors/windowed_G_test.py
@@ -1,6 +1,7 @@
 from data.import_data import import_data
 from Predictors.windowed_G import windowedGClassifier
 from nltk.tokenize import word_tokenize
+import numpy as np
 import pickle
 
 def tokenize(sen):
@@ -9,18 +10,26 @@ def tokenize(sen):
 if __name__ == '__main__':
     tr, te = import_data()
 
-    
-    
-    encoder = Word2Vec([
-        tokenize(s)
-        for s in sentences],
-        size=self.encoder_size,
-        window=self.window,
-        min_count=0,
-        workers=4)
+    text = tr['text'].as_matrix()
+    authors = tr['author'].as_matrix()
 
-    myc = windowedGClassifier(encoder_size=30, DNNlayers=[30, 20], batch_n = 3)
-    myc.fit(tr.text[0:2], tr.author[0:1])
+    author_set = set(authors)
+    author_key = {k: v for k,v in zip(author_set, range(len(author_set)))}
 
-    with open('trained_model.pkl', 'wb') as file:
-        pickle.dump(myc, file)
+    authors = np.array([author_key[a] for a in authors])
+
+    print(text[:20], authors[:20])
+
+    # encoder = Word2Vec([
+    #     tokenize(s)
+    #     for s in sentences],
+    #     size=self.encoder_size,
+    #     window=self.window,
+    #     min_count=0,
+    #     workers=4)
+
+    # myc = windowedGClassifier(encoder_size=30, DNNlayers=[30, 20], batch_n = 3)
+    # myc.fit(tr.text[0:2], tr.author[0:1])
+
+    # with open('trained_model.pkl', 'wb') as file:
+    #     pickle.dump(myc, file)
