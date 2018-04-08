3a22498bbbfb37ddff9ff4447ede5ba606ca0e81
gpeh1g14 <gpeh1g14@soton.ac.uk>
2018-04-08 15:03:29 +0100

    Finalises windowedGClassifier functionality and creates test script.

--- Predictors/windowed_G.py
+++ Predictors/windowed_G.py
@@ -2,14 +2,17 @@ from nltk.tokenize import word_tokenize
 from gensim.models import Word2Vec
 from sklearn.base import BaseEstimator, ClassifierMixin
 import tensorflow as tf
+import numpy as np
+import logging
 
 
 class windowedGClassifier(BaseEstimator, ClassifierMixin):
+
     def __init__(
         self,
         encoder=None,
         window=5,
-        DNNlayers=['250,100']
+        DNNlayers=[100, 80]
     ):
 
         self.encoder = encoder
@@ -22,43 +25,60 @@ class windowedGClassifier(BaseEstimator, ClassifierMixin):
 
     def windowed_inp_data_(self, sentences, labels, wlen):
         data = {'windows': []}
-        labels = []
+        outlabels = []
         for s, l in zip(sentences, labels):
             tokens = self.tokenize_(s)
-            encs = [self.encoder(t) for t in tokens]
+            encs = [self.encoder[t] for t in tokens]
 
             for i in range(len(encs) + 1 - wlen):
                 data['windows'].append(encs[i:i + wlen])
-                labels.append(l)
+                outlabels.append(l)
 
-        return data, labels
+        data['windows'] = np.array(data['windows'])
 
+        authors = set(outlabels)
+        key = {k: v for k, v in zip(authors, range(len(authors)))}
 
+        trans = [key[l] for l in outlabels]
+
+        print('Author to label encoding is:', key)
+
+        return data, trans
 
     def fit(self, sentences, labels):
 
         print("Training word encoding")
 
+        def_enc_size = 100
+
         if self.encoder is None:
             self.encoder = Word2Vec([
                 self.tokenize_(s)
                 for s in sentences],
-                size=250,
+                size=def_enc_size,
                 window=self.window,
-                min_count=1,
+                min_count=0,
                 workers=4)
-            self.encoder.size = 250
+            self.encoder.size = def_enc_size
 
         print("Training DNN")
 
         enc_size = self.encoder.size
 
-        dnn_clf = tf.contrib.learn.DNNClassifier(
+        fc = tf.feature_column.numeric_column(
+            'windows',
+            shape=[self.window, enc_size])
+
+        def input_fn():
+            return self.windowed_inp_data_(sentences, labels, self.window)
+
+        dnn_clf = tf.estimator.DNNClassifier(
             hidden_units=self.DNNlayers,
             n_classes=3,
-            feature_columns=)
+            feature_columns=[fc])
 
-        dnn_clf = tf.contrib.learn.SKCompat(dnn_clf)
+        logging.getLogger().setLevel(logging.INFO)
+        dnn_clf.train(input_fn)
 
         def encoded_data():
             for s in sentences:
--- Predictors/windowed_G_test.py
+++ Predictors/windowed_G_test.py
@@ -1,9 +1,12 @@
 from data.import_data import import_data
-
-
+from Predictors.windowed_G import windowedGClassifier
+import pickle
 
 if __name__ == '__main__':
     tr, te = import_data()
 
-    for s in tr.text[:10]:
-        print(word_tokenize(s))
+    myc = windowedGClassifier()
+    myc.fit(tr.text, tr.author)
+
+    with open('trained_model.pkl', 'wb') as file:
+        pickle.dump(myc, file)
