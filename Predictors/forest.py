689d72275455c641e155cd15c8e7087271f59d47
Tom Eccles <tde1g14@soton.ac.uk>
2018-04-11 15:13:24 +0100

    Randomised parameter search for sklearn models
    Example given for the forrest classifier, which found that the best number of estimators was 437 (accross 100 samples).

--- Predictors/forest.py
+++ Predictors/forest.py
@@ -1,13 +1,18 @@
 from sklearn.ensemble import RandomForestClassifier
-from Predictors.sklearn import SklearnClassifier, test_sklearnclassifier
-
+from Predictors.sklearn import SklearnClassifier, test_sklearnclassifier, random_search_params
+from scipy.stats import expon
 
 
 class ForestClassifier(SklearnClassifier):
     def init_clf_(self, **kwargs):
         self.clf = RandomForestClassifier(
-            n_jobs=-1,
+        #    n_jobs=-1,
             **kwargs)
 
 if __name__ == '__main__':
-    test_sklearnclassifier(ForestClassifier)
+    test_sklearnclassifier(ForestClassifier, n_jobs=-1, n_estimators = 437)
+    param_dist = {
+        'n_estimators': expon(scale=500),
+        }
+
+    #random_search_params(ForestClassifier, param_dist)
--- Predictors/sklearn.py
+++ Predictors/sklearn.py
@@ -1,6 +1,12 @@
 from sklearn.base import BaseEstimator, ClassifierMixin
 from abc import ABC, abstractmethod
 
+from data.import_data import import_data
+from data.padded_sentences import PaddedSentenceTransformer
+from data.numbered_authors import NumberAuthorsTransformer
+from sklearn.model_selection import cross_val_predict, RandomizedSearchCV
+from sklearn.metrics import confusion_matrix, precision_score, recall_score, f1_score, accuracy_score
+
 import numpy as np
 
 # doens't really do much. See data.{padded_sentences, named_authors} for
@@ -44,26 +50,32 @@ class SklearnClassifier(ABC, BaseEstimator, ClassifierMixin):
     def predict(self, X):
         return self.clf.predict(self.reshape_(X))
 
+    def get_params(self, deep=True):
+        return self.clf.get_params(deep)
 
-def test_sklearnclassifier(Clf, train_limit=None, **kwargs):
-    from data.import_data import import_data
-    from data.padded_sentences import PaddedSentenceTransformer
-    from data.numbered_authors import NumberAuthorsTransformer
-    from sklearn.model_selection import cross_val_predict
-    from sklearn.metrics import confusion_matrix, precision_score, recall_score, f1_score, accuracy_score
 
+def get_data_(train_limit):
     tr, te = import_data()
+
+    # cross_val_predict (test_sklearnclassifier) does the train/test/cv split
+    # internally
+    text = list(tr.text) + list(te.text)
+    author = list(tr.author) + list(te.author)
 
     label_enc = NumberAuthorsTransformer()
     data_enc = PaddedSentenceTransformer(encoder_size=100)
 
-    myc = Clf(**kwargs)
-
     if train_limit == None:
-        train_limit = len(tr.text)
+        train_limit = len(text)
 
-    X_train = data_enc.fit_transform(tr.text[:train_limit])
-    y_train = label_enc.fit_transform(tr.author[:train_limit])
+    X = data_enc.fit_transform(text[:train_limit])
+    y = label_enc.fit_transform(author[:train_limit])
+
+    return (X, y)
+
+
+def test_estimator_(myc, train_limit):
+    X_train, y_train = get_data_(train_limit)
 
     print("Running cross-validation...")
     y_train_pred = cross_val_predict(myc, X_train, y_train)
@@ -87,3 +99,24 @@ def test_sklearnclassifier(Clf, train_limit=None, **kwargs):
 
     f1 = f1_score(y_train, y_train_pred, average='micro')
     print("F1 Score: {}".format(f1))
+
+
+def test_sklearnclassifier(Clf, train_limit=None, **kwargs):
+    myc = Clf(**kwargs)
+
+    test_estimator_(myc, train_limit)
+
+
+def random_search_params(Clf, param_dist, train_limit=None, n_iter=100):
+    X, y = get_data_(train_limit)
+    my_clf = Clf()
+
+    search = RandomizedSearchCV(my_clf, param_dist, n_iter=n_iter, n_jobs=-1)
+
+    print("Running search...")
+    search.fit(X, y)
+
+    print("\nBest Parameters: {}".format(search.best_params_))
+
+    print("\nTesting best estimator...")
+    test_estimator_(search.best_estimator_, train_limit)
